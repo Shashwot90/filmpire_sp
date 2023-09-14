8f2d4c7e3acfa4c8146d6de9faa2b719d7650907
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-14 10:34:43 +0545

    folder structures

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 import { Route, Routes } from 'react-router-dom'
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './';
+
 const App = () => (
   <div>
     <CssBaseline />
+    <NavBar />
     <main>
         <Routes>
-            <Route exact element="/movie/:id"><h1>Movie Information</h1></Route>
-            <Route exact element="/actors/:id"><h1>Actors</h1></Route>
-            <Route exact element="/"><h1>Movies</h1></Route>
-            <Route exact element="/profile/:id"><h1>Profile</h1></Route>
+          <Route exact path="/" element={<Movies />} />
+          
+          <Route exact path="/movie/:id" element={<MovieInformation />} />
+          <Route exact path="/actors/:id" element={<Actors />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
         </Routes>
     </main>
   </div>
