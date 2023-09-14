e0892d9f9c495a17907465f1874378dee91cd54c
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-14 10:04:35 +0545

    routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom'
 const App = () => (
-   
-    <div>App</div>
-   
-)
+  <div>
+    <CssBaseline />
+    <main>
+        <Routes>
+            <Route exact element="/movie/:id"><h1>Movie Information</h1></Route>
+            <Route exact element="/actors/:id"><h1>Actors</h1></Route>
+            <Route exact element="/"><h1>Movies</h1></Route>
+            <Route exact element="/profile/:id"><h1>Profile</h1></Route>
+        </Routes>
+    </main>
+  </div>
+);
 
-export default App
+export default App;
