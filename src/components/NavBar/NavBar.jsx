23f2c12b998eaee628360f4fab50d9b7f3ae7947
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-15 07:54:13 +0545

    navbar comp

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar } from '@mui/material';
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
+import { useTheme } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+      <AppBar position=''>
+        <Toolbar className=''>
+
+        </Toolbar>
+      </AppBar>
+    </>
   )
 }
 
