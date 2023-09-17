888c17fc2d9d8361d2b896889cfe37bc5bb06374
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-17 07:16:12 +0545

    store seup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
+import store from "./app/store";
 
 const theme = createTheme({});
 
