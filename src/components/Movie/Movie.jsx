aa117693ad41cee3cc9a849fa18ae87327c949e3
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-18 08:12:38 +0545

    movie jsx and css

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -7,7 +7,9 @@ import useStyles from './styles';
 const Movie = ({ movie, i}) => {
     const classes = useStyles();
   return (
-    <div>Movie</div>
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}> 
+        <Typography className={classes.title} variant='h5'>{movie.title}</Typography>
+    </Grid>
   )
 }
 
