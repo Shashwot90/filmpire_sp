2377d075d95dce071caf8e2c7db0046b4fe62098
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-09-18 07:30:16 +0545

    movie list

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
+
 
 const Movies = () => {
   const { data } = useGetMoviesQuery();
-  console.log(data);
+  //console.log(data);
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data} />
+    </div>
   )
 }
 
